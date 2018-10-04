7ccd0eaac7bd92193f847f5810cb54a354dcb29a
tsv2013 <tsv2013@users.noreply.github.com>
2018-10-04 22:00:49 +0300

    Supported custom functions

--- sources/index.ts
+++ sources/index.ts
@@ -3,6 +3,29 @@ export class Token {
   type: string;
 }
 
+export enum TokenizerStates { Started = 1, ParsingNumber = 2, ParsingFunction = 3, Finished = 4, Error = 5 }
+enum KnownStringComponents { Delimiter = 1, Digit = 2, Other = 3, Bracket = 4 }
+
+var tokenStateMachine: any = {};
+tokenStateMachine[TokenizerStates.Started] = { 
+  1: TokenizerStates.Started,
+  2: TokenizerStates.ParsingNumber,
+  3: TokenizerStates.ParsingFunction,
+  4: TokenizerStates.ParsingFunction
+};
+tokenStateMachine[TokenizerStates.ParsingNumber] = { 
+  1: TokenizerStates.Finished,
+  2: TokenizerStates.ParsingNumber,
+  3: TokenizerStates.Finished,
+  4: TokenizerStates.Finished
+};
+tokenStateMachine[TokenizerStates.ParsingFunction] = { 
+  1: TokenizerStates.Finished,
+  2: TokenizerStates.Finished,
+  3: TokenizerStates.ParsingFunction,
+  4: TokenizerStates.Finished
+};
+
 export class ExpressionEvaluator {
   static operations = {
     "+": {
@@ -24,10 +47,31 @@ export class ExpressionEvaluator {
     "%": {
       priority: 1,
       function: (a, b) => a! % b!
+    },
+    "or": {
+      priority: 0,
+      function: (a, b) => a || b
+    },
+    "and": {
+      priority: 1,
+      function: (a, b) => a && b
+    },
+    "!": {
+      priority: 2,
+      function: (a) => !a
+    },
+    "true": {
+      priority: 100,
+      function: () => true
+    },
+    "false": {
+      priority: 100,
+      function: () => false
     }
   };
-  private static digits = "0123456789";
-  private static brackets = "()";
+  private static digits = "0123456789.";
+  private static brackets = "(){}";
+  private static delimeters = " ,\r\r\n";
 
   private isOfMoreOrEqualPriority(currentOp: string, otherOp: string): boolean {
     return (
@@ -36,36 +80,62 @@ export class ExpressionEvaluator {
     );
   }
 
+  classifySymbol(symbol: string): KnownStringComponents {
+    if(ExpressionEvaluator.delimeters.indexOf(symbol) !== -1) {
+      return KnownStringComponents.Delimiter;
+    } else if(ExpressionEvaluator.brackets.indexOf(symbol) !== -1) {
+      return KnownStringComponents.Bracket;
+    } else if(ExpressionEvaluator.digits.indexOf(symbol) !== -1) {
+      return KnownStringComponents.Digit;
+    }
+    return KnownStringComponents.Other;
+  }
+
+  scanToken(str: string, start: number) {
+    var state: TokenizerStates = TokenizerStates.Started;
+    var workingState = TokenizerStates.Error;
+    var tokenString = "";
+    var i = start;
+    while(i < str.length && (state !== TokenizerStates.Finished && state !== TokenizerStates.Error)) {
+      var symbolClass = this.classifySymbol(str[i]);
+      state = tokenStateMachine[state][symbolClass];
+      if(state === TokenizerStates.ParsingFunction && ExpressionEvaluator.operations[tokenString] !== undefined) {
+        state = TokenizerStates.Finished;
+      }
+      if(state === TokenizerStates.ParsingFunction || state === TokenizerStates.ParsingNumber) {
+        workingState = state;
+        tokenString += str[i++];
+      } else if(state === TokenizerStates.Started) {
+        i++;
+      }
+    }
+    if(tokenString === "") {
+      workingState = TokenizerStates.Error;
+    }
+    return {
+      workingState,
+      tokenString,
+      pos: i
+    }
+  }
+
   tokenize(expression: string): Array<Token> {
     var tokens: Array<Token> = [];
     for (var i = 0; i < expression.length; ) {
-      if (ExpressionEvaluator.brackets.indexOf(expression[i]) !== -1) {
-        tokens.push({ type: expression[i] });
-        i++;
-        continue;
-      }
-      if (
-        Object.keys(ExpressionEvaluator.operations).indexOf(expression[i]) !==
-        -1
-      ) {
-        tokens.push({ type: expression[i] });
-        i++;
-        continue;
-      }
-      if (ExpressionEvaluator.digits.indexOf(expression[i]) !== -1) {
-        var temp = "";
-        for (
-          ;
-          ExpressionEvaluator.digits.indexOf(expression[i]) !== -1 &&
-          i < expression.length;
-          i++
-        ) {
-          temp += expression[i];
+      var tokenCandidate = this.scanToken(expression, i);
+      if(tokenCandidate.workingState !== TokenizerStates.Error) {
+        if(tokenCandidate.workingState === TokenizerStates.ParsingNumber) {
+          tokens.push({
+            type: "n",
+            value: tokenCandidate.tokenString.indexOf(".") !== -1 ? parseFloat(tokenCandidate.tokenString) : parseInt(tokenCandidate.tokenString)
+          });
+        } else {
+          tokens.push({
+            type: tokenCandidate.tokenString
+          });
         }
-        tokens.push({ value: parseInt(temp), type: "n" });
-        continue;
       }
-      i++;
+      i = tokenCandidate.pos;
     }
     return tokens;
   }
@@ -77,19 +147,19 @@ export class ExpressionEvaluator {
 
     var j = 0;
     for (var i = 0; i < tokens.length; i++) {
-      if (tokens[i].type == "n") {
+      if (tokens[i].type === "n") {
         rpn[j++] = tokens[i];
         continue;
       }
-      if (tokens[i].type == "(") {
+      if (tokens[i].type === "(") {
         stack.push(tokens[i]);
         continue;
       }
-      if (tokens[i].type == ")") {
+      if (tokens[i].type === ")") {
         do {
           currToken = stack.pop();
           rpn[j++] = currToken;
-        } while (rpn[j - 1].type != "(");
+        } while (rpn[j - 1].type !== "(");
         j--;
         continue;
       }
@@ -103,12 +173,12 @@ export class ExpressionEvaluator {
             rpn[j++] = currToken;
           } while (
             stack.length > 0 &&
-            this.isOfMoreOrEqualPriority(tokens[i].type, rpn[j - 1].type) &&
-            ExpressionEvaluator.brackets.indexOf(rpn[j - 1].type) === -1
+            ExpressionEvaluator.brackets.indexOf(rpn[j - 1].type) === -1 &&
+            this.isOfMoreOrEqualPriority(tokens[i].type, rpn[j - 1].type)
           );
           if (
-            !this.isOfMoreOrEqualPriority(tokens[i].type, rpn[j - 1].type) ||
-            ExpressionEvaluator.brackets.indexOf(rpn[j - 1].type) !== -1
+            ExpressionEvaluator.brackets.indexOf(rpn[j - 1].type) !== -1 ||
+            !this.isOfMoreOrEqualPriority(tokens[i].type, rpn[j - 1].type)
           ) {
             stack.push(currToken);
             j--;
@@ -127,6 +197,10 @@ export class ExpressionEvaluator {
 
   calculateRPN(rpn: Array<Token>): number {
     var operands: Array<Token> = [];
+
+    if(rpn.length === 0) {
+      return undefined;
+    }
 
     for (var i = 0; i < rpn.length; i++) {
       if (rpn[i].type === "n") {
--- tests/common.test.ts
+++ tests/common.test.ts
@@ -1,12 +1,73 @@
-import { ExpressionEvaluator } from "../sources";
+import { ExpressionEvaluator, TokenizerStates } from "../sources";
 
-test("ExpressionEvaluator basic", () => {
+test("ExpressionEvaluator scan token", () => {
   var ee = new ExpressionEvaluator();
-  expect(ee.evaluate("2*2")).toBe(4);
-  expect(ee.evaluate("3+2*2")).toBe(7);
-  expect(ee.evaluate("6 / 2")).toBe(3);
-  expect(ee.evaluate("6 / 2 + 1 + 2*2 - 5")).toBe(3);
-  expect(ee.evaluate("5 % 3 ")).toBe(2);
+  var tc = ee.scanToken("2*2", 0);
+  expect(tc.tokenString).toBe("2");
+  expect(tc.workingState).toBe(TokenizerStates.ParsingNumber);
+  expect(tc.pos).toBe(1);
+  tc = ee.scanToken("2*2", 1);
+  expect(tc.tokenString).toBe("*");
+  expect(tc.workingState).toBe(TokenizerStates.ParsingFunction);
+  expect(tc.pos).toBe(2);
+  tc = ee.scanToken("2*2", 2);
+  expect(tc.tokenString).toBe("2");
+  expect(tc.workingState).toBe(TokenizerStates.ParsingNumber);
+  expect(tc.pos).toBe(3);
+
+  tc = ee.scanToken("2 * 2", 0);
+  expect(tc.tokenString).toBe("2");
+  expect(tc.workingState).toBe(TokenizerStates.ParsingNumber);
+  expect(tc.pos).toBe(1);
+  tc = ee.scanToken("2 * 2", 1);
+  expect(tc.tokenString).toBe("*");
+  expect(tc.workingState).toBe(TokenizerStates.ParsingFunction);
+  expect(tc.pos).toBe(3);
+  tc = ee.scanToken("2 * 2", 3);
+  expect(tc.tokenString).toBe("2");
+  expect(tc.workingState).toBe(TokenizerStates.ParsingNumber);
+  expect(tc.pos).toBe(5);
+
+  tc = ee.scanToken("", 0);
+  expect(tc.tokenString).toBe("");
+  expect(tc.workingState).toBe(TokenizerStates.Error);
+  expect(tc.pos).toBe(0);
+  tc = ee.scanToken(" ", 0);
+  expect(tc.tokenString).toBe("");
+  expect(tc.workingState).toBe(TokenizerStates.Error);
+  expect(tc.pos).toBe(1);
+  tc = ee.scanToken("   ", 0);
+  expect(tc.tokenString).toBe("");
+  expect(tc.workingState).toBe(TokenizerStates.Error);
+  expect(tc.pos).toBe(3);
+});
+
+test("ExpressionEvaluator tokenize", () => {
+  var ee = new ExpressionEvaluator();
+  var tokens = ee.tokenize("2*2");
+  expect(tokens.length).toBe(3);
+  expect(tokens[0].type).toBe("n");
+  expect(tokens[1].type).toBe("*");
+  expect(tokens[2].type).toBe("n");
+
+  tokens = ee.tokenize("2 * 2");
+  expect(tokens.length).toBe(3);
+  expect(tokens[0].type).toBe("n");
+  expect(tokens[1].type).toBe("*");
+  expect(tokens[2].type).toBe("n");
+});
+
+test("ExpressionEvaluator tokenize ') '", () => {
+  var ee = new ExpressionEvaluator();
+  var tokens = ee.tokenize("5 % (3-1) ");
+  expect(tokens.length).toBe(7);
+  expect(tokens[0].type).toBe("n");
+  expect(tokens[1].type).toBe("%");
+  expect(tokens[2].type).toBe("(");
+  expect(tokens[3].type).toBe("n");
+  expect(tokens[4].type).toBe("-");
+  expect(tokens[5].type).toBe("n");
+  expect(tokens[6].type).toBe(")");
 });
 
 test("ExpressionEvaluator calculateRPN", () => {
@@ -18,4 +79,88 @@ test("ExpressionEvaluator calculateRPN", () => {
       { type: "/" }
     ])
   ).toBe(2);
+});
+
+test("ExpressionEvaluator basic", () => {
+  var ee = new ExpressionEvaluator();
+  expect(ee.evaluate("2*2")).toBe(4);
+  expect(ee.evaluate("3+2*2")).toBe(7);
+  expect(ee.evaluate("6 / 2")).toBe(3);
+  expect(ee.evaluate("6 / 2 + 1 + 2*2 - 5")).toBe(3);
+  expect(ee.evaluate("5 % 3 ")).toBe(2);
+  expect(ee.evaluate("5 % (3-1) ")).toBe(1);
+});
+
+test("ExpressionEvaluator basic logic", () => {
+  var ee = new ExpressionEvaluator();
+  expect(ee.evaluate("true")).toBe(true);
+  expect(ee.evaluate("true and true")).toBe(true);
+  expect(ee.evaluate("false or false")).toBe(false);
+  expect(ee.evaluate("!true")).toBe(false);
+  expect(ee.evaluate("!false")).toBe(true);
+});
+
+test("ExpressionEvaluator custom functions", () => {
+  var ee = new ExpressionEvaluator();
+  ExpressionEvaluator.operations["myFunc"] = {
+    priority: 100,
+    function: (a, b) => (a+b)/2
+  }
+  ExpressionEvaluator.operations["mySq"] = {
+    priority: 100,
+    function: a => a*a
+  }
+  ExpressionEvaluator.operations["mySecond"] = {
+    priority: 100,
+    function: (a, b, c) => b
+  }
+  expect(ee.evaluate("myFunc(2,4)")).toBe(3);
+  expect(ee.evaluate("mySq(2)")).toBe(4);
+  expect(ee.evaluate("myFunc(2,4) + mySq(2)")).toBe(7);
+  expect(ee.evaluate("mySecond(5, myFunc(2,4), mySq(2))")).toBe(3);
+});
+
+test("ExpressionEvaluator tokenize custom functions", () => {
+  var ee = new ExpressionEvaluator();
+  var tokens = ee.tokenize("mySecond(5, myFunc(2,4), mySq(2))");
+  expect(tokens.length).toBe(13);
+  expect(tokens[0].type).toBe("mySecond");
+  expect(tokens[1].type).toBe("(");
+  expect(tokens[2].type).toBe("n");
+  expect(tokens[3].type).toBe("myFunc");
+  expect(tokens[4].type).toBe("(");
+  expect(tokens[5].type).toBe("n");
+  expect(tokens[6].type).toBe("n");
+  expect(tokens[7].type).toBe(")");
+  expect(tokens[8].type).toBe("mySq");
+  expect(tokens[9].type).toBe("(");
+  expect(tokens[10].type).toBe("n");
+  expect(tokens[11].type).toBe(")");
+  expect(tokens[12].type).toBe(")");
+});
+
+test("ExpressionEvaluator RPN custom functions", () => {
+  var ee = new ExpressionEvaluator();
+  var tokens = ee.convertToRPN(ee.tokenize("mySecond(5, myFunc(2,4), mySq(2))"));
+  expect(tokens.length).toBe(7);
+  expect(tokens[0].type).toBe("n");
+  expect(tokens[1].type).toBe("n");
+  expect(tokens[2].type).toBe("n");
+  expect(tokens[3].type).toBe("myFunc");
+  expect(tokens[4].type).toBe("n");
+  expect(tokens[5].type).toBe("mySq");
+  expect(tokens[6].type).toBe("mySecond");
+});
+
+test("ExpressionEvaluator basic float", () => {
+  var ee = new ExpressionEvaluator();
+  expect(ee.evaluate("2.1*2")).toBe(4.2);
+});
+
+test("ExpressionEvaluator basic empty", () => {
+  var ee = new ExpressionEvaluator();
+  expect(ee.evaluate("")).toBe(undefined);
+  expect(ee.evaluate(" ")).toBe(undefined);
+  expect(ee.evaluate("   ")).toBe(undefined);
+  expect(ee.evaluate("0")).toBe(0);
 });
